24da97261e9921dbcba282e51f48ef4d0ed6beaf
Logan <coates.logan1125@gmail.com>
2023-02-08 13:34:20 -0700

    finished History Database

--- app/src/main/java/com/example/workoutapp/FinishActivity.kt
+++ app/src/main/java/com/example/workoutapp/FinishActivity.kt
@@ -2,9 +2,14 @@
 
 package com.example.workoutapp
 
+import java.text.SimpleDateFormat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.workoutapp.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.util.*
 
 
 class FinishActivity : AppCompatActivity() {
@@ -24,5 +29,26 @@ class FinishActivity : AppCompatActivity() {
         binding?.btnFinish?.setOnClickListener {
             finish()
         }
+
+        val dao = (application as WorkOutApp).db.historyDao()
+        addDateToDatabase(dao)
     }
+
+    private fun addDateToDatabase(historyDao: HistoryDao){
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("Date: ", "" +dateTime)
+
+        val sdf = SimpleDateFormat("dd MMM yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date : ", "" + date)
+
+        lifecycleScope.launch {
+            historyDao.insert(HistoryEntity(date))
+            Log.e("Date: ", "Added...")
+        }
+
+    }
+
 }
